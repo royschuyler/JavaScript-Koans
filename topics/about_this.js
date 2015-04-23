e21b46cc6b806e2b2fd73a89691bf62b526ec99f
royschuyler <royschuyler@yahoo.com>
2015-04-22 19:04:36 -0500

    trying to get it working again

--- topics/about_this.js
+++ topics/about_this.js
@@ -4,7 +4,7 @@ test("'this' inside a method", function () {
 	var person = {
 		name: 'bob',
 		intro: function () {
-			return "Hello, my name is " + this.__;
+			return "Hello, my name is " + this.name;
 		} 
 	}
 	equal(person.intro(), "Hello, my name is bob", "If an object has a method can you access properties inside it?");
